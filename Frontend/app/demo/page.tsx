03523c225df5e395832708668bd949026463cef4
Amang2004 <amangupta.ans@gmail.com>
2025-09-19 02:08:30 +0530

    add the automatically working for the chat

---
+++ Frontend/app/demo/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { DemoJobFlow } from "@/components/demo-job-flow"
+
+export default function DemoPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <DemoJobFlow />
+    </div>
+  )
+}
---
+++ Frontend/app/test-job/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { TestJobFlow } from "@/components/test-job-flow"
+
+export default function TestJobPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <TestJobFlow />
+    </div>
+  )
+}
